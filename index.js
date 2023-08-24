f3e921bc01b730cb4712b994f15e63cf0149c608
Uzair Saleem <uzairsaleemdev@gmail.com>
2023-08-25 04:07:23 +0500

    authentication done

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cookieParser = require('cookie-parser')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const app = express()
+const ws = require('ws')
 const User = require('./models/User')
  dotenv.config()
   mongoose.connect(process.env.MONGODB_CONNECTION_STRING).then(conn=>{
@@ -56,7 +57,7 @@ try{
                 res.cookie('token',token,{sameSite:'none',secure:true}).status(200).json({userId:foundUser._id})
             })
         }
-        
+
     }
 
 }
@@ -86,6 +87,46 @@ try{
         throw error
     }
 })
-app.listen(4040,()=>{
+
+  const server= app.listen(4040,()=>{
     console.log('app listening on port 4040')
 })
+
+const wss =   new ws.WebSocketServer({server})
+
+    wss.on('connection',(connection,req)=>{
+
+        console.log('connected')
+
+        const cookies = req.headers.cookie;
+        if(cookies){
+         const tokenCookieString =   cookies.split(';').find((str)=>(str.startsWith('token=')))
+        
+
+         if(tokenCookieString){
+           const token = tokenCookieString.split('=')[1]
+           if(token){
+               console.log(token)
+               jwt.verify(token,jwtSecret,{},(err,data)=>{
+                if(err) throw err
+                const {username, userId} =data
+                connection.username = username;
+                connection.userId = userId
+               })
+            }
+
+         }
+
+        }
+
+        [...wss.clients].forEach((client)=>{
+
+            client.send( 
+                JSON.stringify({
+                    online:[...wss.clients].map((c)=>({userId:c.userId,username:c.username}))
+                })
+            )
+        })
+
+
+    })
